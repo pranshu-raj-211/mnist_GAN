9ca3464758dc43a324632e5a73cda5d1a7d271e5
Kaito117 <pranshuraj65536@gmail.com>
2024-02-03 00:08:21 +0530

    fixed some linter issues

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+import io
+import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
-import io
-import logging
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 model = load_model("mnist_generator.h5")
-latent_dim = 128
+LATENT_DIM = 128
 np.random.seed(42)
 logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(message)s",
@@ -22,16 +23,25 @@ logging.basicConfig(
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    """
+    Home page"""
     return templates.TemplateResponse("predict.html", {"request": request})
 
 
 def sample_noise(n_samples: int, latent_dim: int):
+    """
+    Samples noise in the shape of (n_samples, latent_dim)."""
     return np.random.rand(n_samples * latent_dim)
 
 
 @app.post("/predict/")
 async def predict():
-    input_noise = sample_noise(1, latent_dim).reshape((1, latent_dim))
+    """
+    Returns a generated image from the model.
+
+    Samples noise from a normal distribution and passes it through the
+    generator model to get a generated image."""
+    input_noise = sample_noise(1, LATENT_DIM).reshape((1, LATENT_DIM))
     prediction = model.predict(input_noise) * 255
     logging.info("Got predictions array")
     prediction = prediction.reshape(28, 28)
