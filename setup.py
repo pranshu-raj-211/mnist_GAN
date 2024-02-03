75ec503724b5e33d58cef60d39adc8d9df58c56d
Kaito117 <pranshuraj65536@gmail.com>
2024-02-03 11:37:16 +0530

    add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="generate_digits",
+    version="1.0",
+    packages=find_packages(),
+    author="pranshu-raj-211",
+    install_requires=[
+        "numpy",
+        "tensorflow",
+        "Pillow",
+        "fastapi",
+    ],
+)
