cd6ab6967d4a9cc4a84bfbb49257004d2ed9457a
Kaito117 <pranshuraj65536@gmail.com>
2024-02-10 00:06:05 +0530

    fix pylint errors

--- client.py
+++ client.py
@@ -6,11 +6,10 @@ import base64
 import io
 import json
 from flask import Flask, jsonify, render_template
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 import requests
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 logging.basicConfig(level=logging.INFO)
 LATENT_DIM = 128
@@ -21,7 +20,6 @@ app = Flask(__name__)
 def home():
     """
     Home Endpoint"""
-    logging.warning("At home")
     return render_template("index.html")
 
 
@@ -30,19 +28,15 @@ def home():
 def predict():
     """
     Prediction Endpoint"""
-    logging.warning("Request received")
+    logging.info("Request Received, predicting ...")
     input_data = np.random.rand(1, LATENT_DIM).tolist()
 
-    # Convert the data to the format expected by TensorFlow Serving
     data = {
         "signature_name": "serving_default",
         "instances": input_data,
     }
-
-    # Convert the data to JSON
     data = json.dumps(data)
 
-    # Send a POST request to the TensorFlow Serving model server
     headers = {"content-type": "application/json"}
     json_response = requests.post(
         "http://localhost:8501/v1/models/my_model:predict",
@@ -51,27 +45,13 @@ def predict():
         timeout=60,
     )
 
-    # Parse the response
     predictions = json.loads(json_response.text)["predictions"]
-
-    # Convert the predictions to a numpy array
     predictions = np.array(predictions)
-
-    # Reshape the predictions if necessary
     predictions = predictions.reshape((28, 28))
-
-    # Create a BytesIO object
     img_io = io.BytesIO()
 
-    # Save the image to the BytesIO object
     plt.imsave(img_io, predictions, cmap="gray", format="png")
-    # plt.imshow(predictions)
-    # plt.show()
-
-    # Get the base64 encoding of the image
     img_base64 = base64.b64encode(img_io.getvalue()).decode("utf-8")
-
-    # Return the base64 image in the response
     return jsonify({"image": img_base64})
 
 
